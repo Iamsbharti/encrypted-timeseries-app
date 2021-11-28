abc13234243890be0b5943820251212e68b620ff
Saurabh Bharti <saurabhbharti9@gmail.com>
2021-11-28 15:19:38 +0530

    heroku prod deploy

--- server-enc/server.js
+++ server-enc/server.js
@@ -23,6 +23,18 @@ app.all("*", function (req, res, next) {
   next();
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "../client-enc/build")));
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../client-enc/build", "index.html")
+    );
+  });
+}
+
 //server Listener
 const port = process.env.PORT;
 const server = app.listen(port, () =>
